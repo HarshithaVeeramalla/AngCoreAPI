ffd89bd93fa1bbff51406e59e9bb8ca9600264c9
Robert R <rarDevelopment@users.noreply.github.com>
2023-03-11 00:36:55 -0500

    sample oauth setup

--- rardk.web/rardk.web.UI/src/app/app.component.ts
+++ rardk.web/rardk.web.UI/src/app/app.component.ts
@@ -1,21 +1,50 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { JwksValidationHandler, OAuthService } from 'angular-oauth2-oidc';
+import { authCodeFlowConfig } from './sso.config';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   public forecasts?: WeatherForecast[];
 
-  constructor(http: HttpClient) {
-    http.get<WeatherForecast[]>('/api/weatherforecast').subscribe(result => {
-      this.forecasts = result;
-    }, error => console.error(error));
-    http.get('/api/now').subscribe(result => {
-      console.log("done", result);
-    }, error => console.error(error));
+  constructor(http: HttpClient, private oauthService: OAuthService) {
+    this.configureSingleSignOn();
+    http.get<WeatherForecast[]>('/api/weatherforecast').subscribe(
+      (result) => {
+        this.forecasts = result;
+      },
+      (error) => console.error(error)
+    );
+    http.get('/api/now').subscribe(
+      (result) => {
+        console.log('done', result);
+      },
+      (error) => console.error(error)
+    );
+  }
+
+  login() {
+    this.oauthService.initLoginFlow();
+  }
+
+  logout() {
+    this.oauthService.logOut();
+    //this.oauthService.revokeTokenAndLogout();
+  }
+
+  get token() {
+    let claims: any = this.oauthService.getIdentityClaims();
+    return claims ? claims : null;
+  }
+
+  configureSingleSignOn() {
+    this.oauthService.configure(authCodeFlowConfig);
+    this.oauthService.tokenValidationHandler = new JwksValidationHandler();
+    this.oauthService.loadDiscoveryDocumentAndTryLogin();
   }
 
   title = 'rardk.web.UI';
--- rardk.web/rardk.web.UI/src/app/app.module.ts
+++ rardk.web/rardk.web.UI/src/app/app.module.ts
@@ -3,15 +3,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { OAuthModule } from 'angular-oauth2-oidc';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HomeComponent],
   imports: [
-    BrowserModule, HttpClientModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    OAuthModule.forRoot(),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
